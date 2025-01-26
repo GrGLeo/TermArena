c1ca3bf68a1f63928b47c68dea51ba22b9a049a0
GrGLeo <graziani.gothe.leo@gmail.com>
2025-01-26 12:55:29 +0100

    add: lobby model

---
+++ client/model/lobby.go
@@ -0,0 +1,138 @@
+package model
+
+import (
+	"log"
+	"net"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type LobbyModel struct {
+	options       []string
+	selected      int
+	width, height int
+	conn          *net.TCPConn
+}
+
+func NewLobbyModel(conn *net.TCPConn) LobbyModel {
+
+	return LobbyModel{
+		options:  []string{"Solo (1 player 1 bot vs 2 bots)", "2 players vs 2 bots", "2 players vs 2 players"},
+		selected: 0,
+		conn:     conn,
+	}
+}
+
+func (m *LobbyModel) SetDimension(height, width int) {
+	m.height = height
+	m.width = width
+}
+
+func (m LobbyModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m LobbyModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEsc, tea.KeyCtrlC:
+			return m, tea.Quit
+		case tea.KeyEnter:
+			selectedOption := m.options[m.selected]
+			log.Println("Selected option:", selectedOption)
+			return m, nil
+		}
+
+		switch msg.String() {
+		case "k":
+			if m.selected == 0 {
+				m.selected = len(m.options) - 1
+			} else {
+				m.selected--
+			}
+			return m, nil
+		case "j":
+			if m.selected == len(m.options)-1 {
+				m.selected = 0
+			} else {
+				m.selected++
+			}
+			return m, nil
+		}
+	}
+	return m, cmd
+}
+
+func (m LobbyModel) View() string {
+	// Build the options list
+	var optionsBuilder strings.Builder
+	selectedChar := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("205")).
+		Render("> ")
+
+	for i, opt := range m.options {
+		if m.selected == i {
+			optionsBuilder.WriteString(selectedChar)
+			optionsBuilder.WriteString(
+				lipgloss.NewStyle().
+					Foreground(lipgloss.Color("205")).
+					Bold(true).
+					Render(opt),
+			)
+		} else {
+			optionsBuilder.WriteString("  ")
+			optionsBuilder.WriteString(
+				lipgloss.NewStyle().
+					Foreground(lipgloss.Color("240")).
+					Render(opt),
+			)
+		}
+		optionsBuilder.WriteString("\n")
+	}
+
+	// Game instructions
+	gameInstruction := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("255")).
+		Render(
+			"Capture the flag is a 2v2 multiplayer game.\n" +
+				"Each team needs to capture the enemy flag and bring it back to their own base.\n" +
+				"Player can move around the map using w,a,s,d.\n" +
+				"Player can dash using space. Dash moves the player instantly by a short distance.\n" +
+				"Dash passes through walls.",
+		)
+
+	optionsStyle := lipgloss.NewStyle().
+		Width(50).
+		Align(lipgloss.Left).
+		Border(lipgloss.NormalBorder(), true, false, false, false).
+		Padding(1, 2)
+
+	instructionsStyle := lipgloss.NewStyle().
+		Width(50).
+		Align(lipgloss.Left).
+		Border(lipgloss.NormalBorder(), true, false, false, true).
+		Padding(1, 2)
+
+	layout := lipgloss.JoinHorizontal(
+		lipgloss.Top,
+		optionsStyle.Render(optionsBuilder.String()),
+		instructionsStyle.Render(gameInstruction),
+	)
+
+	return lipgloss.Place(
+		m.width,
+		m.height,
+		lipgloss.Center,
+		lipgloss.Center,
+		lipgloss.NewStyle().
+			Padding(2, 4).
+			Render(layout),
+	)
+}
