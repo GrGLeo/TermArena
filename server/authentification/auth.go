4d87a8f9f8eb08f8b199e449930b00dc2b5c85b7
GrGLeo <graziani.gothe.leo@gmail.com>
2025-01-28 19:31:28 +0100

    fix: addding dash to all player and remove print

--- server/authentification/auth.go
+++ server/authentification/auth.go
@@ -1,12 +1,9 @@
 package auth
 
 import (
-	"fmt"
-
 	"github.com/GrGLeo/ctf/server/event"
 )
 
 func Authentificate(msg event.Message) event.Message {
-  fmt.Println("auth user")
   return event.AuthMessage{Success: 1}
 }
