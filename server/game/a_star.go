7bb2560fbe0e922119867685c9b659feee686294
GrGLeo <graziani.gothe.leo@gmail.com>
2025-02-23 19:08:45 +0100

    wip: a*

--- server/game/a_star.go
+++ server/game/a_star.go
@@ -1,47 +1,47 @@
 package game
-
-type Node struct {
-	x, y    int
-	g, h, f int
-	parent  *Node
-	index   int
-}
-
-type PriorityQueue []*Node
-
-func (pq PriorityQueue) Len() int {
-  return len(pq)
-}
-
-func (pq PriorityQueue) Less(i, j int) bool {
-  return pq[i].f < pq[j].f
-}
-
-func (pq PriorityQueue) Swap(i, j int) {
-  pq[i], pq[j] = pq[j], pq[i]
-  pq[i].index = i
-  pq[j].index = j
-}
-
-func (pq *PriorityQueue) Push(x interface{}) {
-  n := len(*pq)
-  node := x.(*Node)
-  node.index = n
-  *pq = append(*pq, node)
-}
-
-func (pq *PriorityQueue) Pop() interface{} {
-  old := *pq
-  n := len(old)
-  node := old[n-1]
-  node.index = -1
-  *pq = old[0: n-1]
-  return node
-}
-
-func heuristic(a, b *Node) int {
-  return Absolute(a.x - b.x) + Absolute(a.y - b.y)
-}
+// 
+// type Node struct {
+// 	x, y    int
+// 	g, h, f int
+// 	parent  *Node
+// 	index   int
+// }
+// 
+// type PriorityQueue []*Node
+// 
+// func (pq PriorityQueue) Len() int {
+//   return len(pq)
+// }
+// 
+// func (pq PriorityQueue) Less(i, j int) bool {
+//   return pq[i].f < pq[j].f
+// }
+// 
+// func (pq PriorityQueue) Swap(i, j int) {
+//   pq[i], pq[j] = pq[j], pq[i]
+//   pq[i].index = i
+//   pq[j].index = j
+// }
+// 
+// func (pq *PriorityQueue) Push(x interface{}) {
+//   n := len(*pq)
+//   node := x.(*Node)
+//   node.index = n
+//   *pq = append(*pq, node)
+// }
+// 
+// func (pq *PriorityQueue) Pop() interface{} {
+//   old := *pq
+//   n := len(old)
+//   node := old[n-1]
+//   node.index = -1
+//   *pq = old[0: n-1]
+//   return node
+// }
+// 
+// func heuristic(a, b *Node) int {
+//   return Absolute(a.x - b.x) + Absolute(a.y - b.y)
+// }
 
 //func aStarSearch(grid *Board, start, goal *Node) []*Node {
 //neighbors := [][2]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
