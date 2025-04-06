576fbdcd1a9bcd910ee24d185011889bb30aa0f8
GrGLeo <graziani.gothe.leo@gmail.com>
2025-04-06 10:38:27 +0200

    add: rust game idea

---
+++ game/src/main.rs
@@ -0,0 +1,19 @@
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct CliArgs {
+    #[arg(long = "port", value_name = "PORT", value_parser = clap::value_parser!(u16))]
+    port: u16,
+
+    #[arg(long = "map", value_name = "MAP_ID", value_parser = clap::value_parser!(u8))]
+    map_id: u8,
+}
+fn main() {
+    let args = CliArgs::parse();
+
+    println!("Starting server...");
+    println!("Port: {}", args.port);
+    println!("Map_id: {}", args.map_id);
+}
