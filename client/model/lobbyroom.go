93b8cb8cc2958517de033af8ccad4e368ac3bef4
GrGLeo <graziani.gothe.leo@gmail.com>
2025-10-16 17:48:25 +0200

    add: case for lookingforroom in main modelb

--- client/model/lobbyroom.go
+++ client/model/lobbyroom.go
@@ -188,6 +188,7 @@ func (m LobbyRoomModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 						m.updateViewport()
 						return m, nil
 					}
+					log.Printf("[CLIENT] Quit packet sent successfully for user %s in room %d", m.username, m.roomID)
 					m.updateViewport()
 					return m, nil
 				}
