7498a8c60fe7e9f88580c5ce472f99d9df3462e3
GrGLeo <graziani.gothe.leo@gmail.com>
2025-10-19 16:56:03 +0200

    fix: visible_map in champion

--- services/game/src/game/entities/champion.rs
+++ services/game/src/game/entities/champion.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::ops::Add;
 use std::time::{Duration, Instant};
 use std::usize;
@@ -98,7 +98,6 @@ pub struct Champion {
     pub row: u16,
     pub col: u16,
     pub direction: Direction,
-    pub visible_map: HashSet<(u16, u16)>,
 }
 
 impl Champion {
@@ -152,7 +151,6 @@ impl Champion {
             row,
             col,
             direction: Direction::Up,
-            visible_map: HashSet::new(),
         }
     }
 
